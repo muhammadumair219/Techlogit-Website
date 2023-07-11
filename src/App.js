b5cc70b72d44a70d031e69b5c745608d2ce86216
Muhammad Umair <brightstar5.mu@gmail.com>
2023-07-11 19:20:02 +0500

    adobe installed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import "./index.scss"
 import Contact from "./utils/Contact/Contact";
+import ServicePage from "./pages/ServicePage";
+
+
 
 function App() {
 
   return (
-    <div className="App position-relative" >
-        <Home />
-        <Contact />
-    </div>
+<>
+<Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route exact path="/services" element={<ServicePage/>}></Route>
+      </Routes>
+</>
   );
 }
 
---
+++ src/pages/ServicePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServicePage() {
+  return (
+    <div>
+      <h3>Service</h3>
+    </div>
+  )
+}
+
+export default ServicePage
